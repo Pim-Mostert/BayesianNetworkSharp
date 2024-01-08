9ee99a9f9e4b1400d5931401148c35627906312c
Pim-Mostert <mostert.pim@gmail.com>
2024-01-08 20:35:59 +0000

    wip

--- BayesianNetwork/BayesianNetwork.cs
+++ BayesianNetwork/BayesianNetwork.cs
@@ -1,20 +1,14 @@
 ﻿namespace BayesianNetwork;
 
-public class BayesianNetwork
+public class BayesianNetwork(ISet<Node> nodes, IDictionary<Node, Node> parents)
 {
-    public IReadOnlySet<Node> Nodes { get; init; }
-    public IDictionary<Node, Node> Parents { get; init; }
-    public IReadOnlyDictionary<Node, Node[]> Children { get; init; }
-
-    public BayesianNetwork(ISet<Node> nodes, IDictionary<Node, Node> parents)
-    {
-        Nodes = new HashSet<Node>(nodes);
-        Parents = parents;
-        Children = nodes.ToDictionary(
+    public IReadOnlySet<Node> Nodes { get; init; } = new HashSet<Node>(nodes);
+    public IDictionary<Node, Node> Parents { get; init; } = parents;
+    public IReadOnlyDictionary<Node, Node[]> Children { get; init; } =
+        nodes.ToDictionary(
             n => n,
             n => parents
                     .Where(kvp => kvp.Value == n)
                     .Select(kvp => kvp.Key)
                     .ToArray());
-    }
 }
--- BayesianNetwork/Node.cs
+++ BayesianNetwork/Node.cs
@@ -1,6 +1,8 @@
+using TorchSharp;
+
 namespace BayesianNetwork;
 
 public class Node
 {
-
+    public required torch.Tensor Cpt { get; set; }
 }
