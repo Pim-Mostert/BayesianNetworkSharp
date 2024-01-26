4b09e373918544387b8054c9b98ddee30dd06c1b
Pim-Mostert <mostert.pim@gmail.com>
2024-01-26 20:09:09 +0000

    First setup

---
+++ src/BayesianNetwork.Inference.SumProduct/SumProductInferenceMachine.cs
@@ -0,0 +1,19 @@
+﻿using BayesianNetwork.Inference.Abstractions;
+using TorchSharp;
+
+namespace BayesianNetwork.Inference.SumProduct;
+
+public class SumProductInferenceMachine : IInferenceMachine
+{
+    public double LogLikelihood => throw new NotImplementedException();
+
+    public void EnterEvidence(Evidence evidence)
+    {
+        throw new NotImplementedException();
+    }
+
+    public torch.Tensor Infer(Node node, bool includeParents = false)
+    {
+        throw new NotImplementedException();
+    }
+}
