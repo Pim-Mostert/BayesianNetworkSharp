c3584a331e20deebdeaa30c6663e888f805a6c9d
Pim-Mostert <mostert.pim@gmail.com>
2024-01-27 19:51:15 +0000

    wip

--- src/BayesianNetwork/Node.cs
+++ src/BayesianNetwork/Node.cs
@@ -1,20 +1,20 @@
-using TorchSharp;
+using BayesianNetwork.Tensors;
 
 namespace BayesianNetwork;
 
 public class Node
 {
-    public Node(torch.Tensor cpt, List<Node>? parents = null, bool isObserved = false, string name = "<unnamed>")
+    public Node(Tensor<State> cpt, List<Node>? parents = null, bool isObserved = false, string name = "<unnamed>")
     {
         parents ??= [];
 
         if (parents.Count != parents.Distinct().Count())
             throw new ArgumentException("May only contain unique elements", nameof(parents));
 
-        if (parents.Count != (cpt.shape.Length - 1))
+        if (parents.Count != (cpt.Value.shape.Length - 1))
             throw new ArgumentException("Size of cpt does not match number of parents", nameof(cpt));
 
-        if (parents.Zip(cpt.shape[..^1]).Any(x => x.First.NumStates != x.Second))
+        if (parents.Zip(cpt.Value.shape[..^1]).Any(x => x.First.NumStates != x.Second))
             throw new ArgumentException("Dimensionality of cpt does not match number of states of parents", nameof(cpt));
 
         Parents = parents;
@@ -24,10 +24,10 @@ public class Node
     }
 
     public IReadOnlyList<Node> Parents { get; init; }
-    public torch.Tensor Cpt { get; init; }
+    public Tensor Cpt { get; init; }
     public bool IsObserved { get; init; }
 
-    public long NumStates => Cpt.shape.Last();
+    public long NumStates => Cpt.Value.shape.Last();
 
     public string Name { get; init; }
     public override string ToString() => Name;
---
+++ src/BayesianNetwork/Tensors/Tensor.cs
@@ -0,0 +1,33 @@
+﻿using TorchSharp;
+
+namespace BayesianNetwork.Tensors;
+
+public class Tensor
+{
+    public torch.Tensor Value { get; private set; }
+
+    public Tensor(torch.Tensor value, long numDims)
+    {
+        if (value.shape.Length != numDims)
+            throw new ArgumentException("Tensor was expected to have {numDims} dimensions", nameof(value));
+
+        Value = value;
+    }
+}
+
+public class Tensor<D1>(torch.Tensor value) : Tensor(value, numDims: 1) { }
+public class Tensor<D1, D2>(torch.Tensor value) : Tensor(value, numDims: 2) { }
+public class Tensor<D1, D2, D3>(torch.Tensor value) : Tensor(value, numDims: 3) { }
+public class Tensor<D1, D2, D3, D4>(torch.Tensor value) : Tensor(value, numDims: 4) { }
+public class Tensor<D1, D2, D3, D4, D5>(torch.Tensor value) : Tensor(value, numDims: 5) { }
+public class Tensor<D1, D2, D3, D4, D5, D6>(torch.Tensor value) : Tensor(value, numDims: 6) { }
+public class Tensor<D1, D2, D3, D4, D5, D6, D7>(torch.Tensor value) : Tensor(value, numDims: 7) { }
+public class Tensor<D1, D2, D3, D4, D5, D6, D7, D8>(torch.Tensor value) : Tensor(value, numDims: 8) { }
+public class Tensor<D1, D2, D3, D4, D5, D6, D7, D8, D9>(torch.Tensor value) : Tensor(value, numDims: 9) { }
+public class Tensor<D1, D2, D3, D4, D5, D6, D7, D8, D9, D10>(torch.Tensor value) : Tensor(value, numDims: 10) { }
+public class Tensor<D1, D2, D3, D4, D5, D6, D7, D8, D9, D10, D11>(torch.Tensor value) : Tensor(value, numDims: 11) { }
+public class Tensor<D1, D2, D3, D4, D5, D6, D7, D8, D9, D10, D11, D12>(torch.Tensor value) : Tensor(value, numDims: 12) { }
+public class Tensor<D1, D2, D3, D4, D5, D6, D7, D8, D9, D10, D11, D12, D13>(torch.Tensor value) : Tensor(value, numDims: 13) { }
+public class Tensor<D1, D2, D3, D4, D5, D6, D7, D8, D9, D10, D11, D12, D13, D14>(torch.Tensor value) : Tensor(value, numDims: 14) { }
+public class Tensor<D1, D2, D3, D4, D5, D6, D7, D8, D9, D10, D11, D12, D13, D14, D15>(torch.Tensor value) : Tensor(value, numDims: 15) { }
+public class Tensor<D1, D2, D3, D4, D5, D6, D7, D8, D9, D10, D11, D12, D13, D14, D15, D16>(torch.Tensor value) : Tensor(value, numDims: 16) { }
